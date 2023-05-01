d566341b4759fd46efb03a533aaf182e3e62ec10
sayed8901 <sayed91515@gmail.com>
2023-05-02 01:14:06 +0600

    image link updated in json data file

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const chefData = require('./data/chef.json');
 
 app.get('/', (req, res) => {
-    res.send('hello from server')
-})
+    res.send('Hello from BD chef server...');
+});
+
+app.get('/chef', (req, res) => {
+    res.send(chefData.chefs);
+});
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(`I need chef data for id#: ${id}`);
+    const singleChefData = chefData?.chefs.find(item => item.id === id) || {};
+    res.send(singleChefData);
+});
 
 
 app.listen(port, () => {
     console.log(`chefs recipe server is running at ${port} port`);
-})
+});
